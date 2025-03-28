0bb9131207edf82e970499128715d6cbd67c16bd
vncs06 <gabrielsilvaa1610@gmail.com>
2025-03-28 13:32:16 -0400

    Adicionar arquivos de projeto.

---
+++ video-downloader/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace video_downloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtUrl = new TextBox();
+            button1 = new Button();
+            progressBar1 = new ProgressBar();
+            folderBrowserDialog1 = new FolderBrowserDialog();
+            SuspendLayout();
+            // 
+            // txtUrl
+            // 
+            txtUrl.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            txtUrl.Location = new Point(12, 12);
+            txtUrl.Name = "txtUrl";
+            txtUrl.PlaceholderText = "Cole o link aqui";
+            txtUrl.Size = new Size(712, 23);
+            txtUrl.TabIndex = 0;
+            txtUrl.TextChanged += textBox1_TextChanged;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(12, 41);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 1;
+            button1.Text = "Download";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            progressBar1.Location = new Point(101, 41);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(623, 23);
+            progressBar1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(736, 243);
+            Controls.Add(progressBar1);
+            Controls.Add(button1);
+            Controls.Add(txtUrl);
+            Name = "Form1";
+            Text = "YoutubeDownloarder";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtUrl;
+        private Button button1;
+        private ProgressBar progressBar1;
+        private FolderBrowserDialog folderBrowserDialog1;
+    }
+}
---
+++ video-downloader/Form1.cs
@@ -0,0 +1,103 @@
+using System.Diagnostics;
+using System.IO;
+
+namespace video_downloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string url = txtUrl.Text;
+            if (string.IsNullOrWhiteSpace(url))
+            {
+                MessageBox.Show("Por favor, insira um link válido!", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            if (folderDialog.ShowDialog() == DialogResult.OK)
+            {
+                string outputPath = Path.Combine(folderDialog.SelectedPath, "%(title)s.%(ext)s");
+
+                ProcessStartInfo psi = new ProcessStartInfo
+                {
+                    FileName = @"C:\yt-dlp.exe",
+                    Arguments = $" -f 270+140 --merge-output-format mp4 --progress --newline --no-simulate \"{url}\" -o \"{outputPath}\"",
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                Process process = new Process { StartInfo = psi };
+                process.OutputDataReceived += (sender, e) =>
+                {
+                    if (!string.IsNullOrEmpty(e.Data))
+                    {
+                        AtualizarProgresso(e.Data);
+                        Console.WriteLine(e.Data);
+                    }
+                };
+
+                process.ErrorDataReceived += (sender, e) =>
+                {
+                    if (!string.IsNullOrEmpty(e.Data))
+                    {
+                        Console.WriteLine("Erro: " + e.Data); 
+                    }
+                };
+
+                try
+                {
+                    
+                    await Task.Run(() =>
+                    {
+                        process.Start();
+                        process.BeginOutputReadLine();
+                        process.BeginErrorReadLine();
+                        process.WaitForExit();
+                    });
+
+                    
+                    MessageBox.Show("Download concluído!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Erro ao iniciar o download: {ex.Message}", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+
+
+        private void AtualizarProgresso(string linha)
+        {
+            Console.WriteLine(linha); 
+
+            if (linha.Contains("[download]"))
+            {
+                var match = System.Text.RegularExpressions.Regex.Match(linha, @"(\d+(\.\d+)?)%");
+                if (match.Success)
+                {
+                    double progresso = double.Parse(match.Groups[1].Value);
+                    progressBar1.Invoke((MethodInvoker)(() => progressBar1.Value = Math.Min(100, (int)progresso)));
+                }
+            }
+        }
+    }
+}
